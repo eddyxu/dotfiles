25fbe68da1e3f2446f3b7357bd54e11b5aa127af
Lei Xu <eddyxu@gmail.com>
2012-03-14 13:10:53 -0500

    + added global git configuration

---
+++ git/README.mk
@@ -0,0 +1,7 @@
+ Git Configuration
+----------------------------
+
+1. Firstly move git/gitignore to ~/.gitignore
+2. Then run
+  $ git config --global user.name "YOUR NAME"
+  $ git config --global user.email "yourname@email.com"
