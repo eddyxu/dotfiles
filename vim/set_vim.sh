59eb8332f0b916fa8d1c6d9a12b5c4a1a2255490
Lei Xu <eddyxu@gmail.com>
2012-03-15 12:26:48 -0500

    * set vim in a pathogen way

--- vim/set_vim.sh
+++ vim/set_vim.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
+# Setting up vim on a new machine
+
+if [ ! -d ~/.vim/bundle ]; then
+	mkdir ~/.vim/{autoload,bundle}
+fi
+cd ~/.vim
+
+curl -so ~/.vim/autoload/pathogen.vim \
+	    https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
+
+git init
+git submodule add https://github.com/mitechie/pyflakes-pathogen.git bundle/pyflakes
+git submodule add https://github.com/reinh/vim-makegreen bundle/makegreen
+git submodule add https://github.com/sontek/rope-vim.git bundle/ropevim
+git submodule add https://github.com/vim-scripts/The-NERD-tree.git bundle/nerdtree
+git submodule add https://github.com/vim-scripts/pep8.git bundle/pep8
+git submodule add git://github.com/vim-scripts/taglist.vim.git bundle/taglist
+git submodule add git://github.com/vim-scripts/Pydiction.git bundle/pydiction
+git submodule init
+git submodule update
+git submodule foreach git submodule init
+git submodule foreach git submodule update
 
 BASEDIR=`dirname $0`
 cd `dirname $0`
@@ -7,7 +29,6 @@ if ! grep -E 'source .+/vimrc' ~/.vimrc > /dev/null; then
 	echo "source $PWD/vimrc" >> ~/.vimrc
 fi
 
-cd ~/.vim
 # make tags for C++
 ctags -f tags -R /usr/include /usr/local/include
 
