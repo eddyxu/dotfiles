db61817e790ca5b2d61b2585224f7d3a31aa03f9
Lei Xu <eddyxu@gmail.com>
2013-03-27 00:56:16 -0500

    + power line

--- vim/set_vim.sh
+++ vim/set_vim.sh
@@ -19,18 +19,19 @@ curl -so ~/.vim/autoload/pathogen.vim \
 	    https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
 
 git init
+git submodule add git://github.com/Lokaltog/powerline.git bundle/powerline
 git submodule add git://github.com/altercation/vim-colors-solarized.git bundle/solarized
-git submodule add git://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
 git submodule add git://github.com/ervandew/supertab.git bundle/supertab
 git submodule add git://github.com/funorpain/vim-cpplint.git bundle/cpplint
+git submodule add git://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
 git submodule add git://github.com/lambdalisue/nose.vim.git bundle/node
 git submodule add git://github.com/majutsushi/tagbar.git bundle/tagbar
 git submodule add git://github.com/rodjek/vim-puppet.git bundle/puppet
 git submodule add git://github.com/tpope/vim-fugitive.git bundle/fugitive
 git submodule add git://github.com/vim-scripts/Pydiction.git bundle/pydiction
+git submodule add git://github.com/vim-scripts/a.vim.git bundle/a.vim
 git submodule add git://github.com/vim-scripts/cscope_macros.vim.git bundle/cscope
 git submodule add git://github.com/vim-scripts/taglist.vim.git bundle/taglist
-git submodule add git://github.com/Lokaltog/vim-powerline.git bundle/powerline
 git submodule add https://github.com/hynek/vim-python-pep8-indent.git bundle/pep8-indent
 git submodule add https://github.com/mitechie/pyflakes-pathogen.git bundle/pyflakes
 git submodule add https://github.com/reinh/vim-makegreen bundle/makegreen
