b3bcb7d42aee70e28079324580bf7f80c76b8962
Lei Xu <eddyxu@gmail.com>
2013-07-10 10:45:30 -0500

    * Change to use vundle

--- vim/set_vim.sh
+++ vim/set_vim.sh
@@ -13,36 +13,9 @@ fi
 for d in `find $ROOTDIR/vim -maxdepth 1 -type d`; do
   cp -rf $d ~/.vim
 done
-cd ~/.vim
 
-curl -so ~/.vim/autoload/pathogen.vim \
-	    https://raw.github.com/tpope/vim-pathogen/HEAD/autoload/pathogen.vim
-
-git init
-git submodule add git://github.com/Lokaltog/powerline.git bundle/powerline
-git submodule add git://github.com/Rip-Rip/clang_complete.git bundle/clang_complete
-git submodule add git://github.com/altercation/vim-colors-solarized.git bundle/solarized
-git submodule add git://github.com/ervandew/supertab.git bundle/supertab
-git submodule add git://github.com/funorpain/vim-cpplint.git bundle/cpplint
-git submodule add git://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
-git submodule add git://github.com/lambdalisue/nose.vim.git bundle/node
-git submodule add git://github.com/majutsushi/tagbar.git bundle/tagbar
-git submodule add git://github.com/rodjek/vim-puppet.git bundle/puppet
-git submodule add git://github.com/tpope/vim-fugitive.git bundle/fugitive
-git submodule add git://github.com/vim-scripts/Pydiction.git bundle/pydiction
-git submodule add git://github.com/vim-scripts/a.vim.git bundle/a.vim
-git submodule add git://github.com/vim-scripts/cscope_macros.vim.git bundle/cscope
-git submodule add git://github.com/vim-scripts/taglist.vim.git bundle/taglist
-git submodule add https://github.com/hynek/vim-python-pep8-indent.git bundle/pep8-indent
-git submodule add https://github.com/mitechie/pyflakes-pathogen.git bundle/pyflakes
-git submodule add https://github.com/reinh/vim-makegreen bundle/makegreen
-git submodule add https://github.com/sontek/rope-vim.git bundle/ropevim
-git submodule add https://github.com/vim-scripts/The-NERD-tree.git bundle/nerdtree
-git submodule add https://github.com/vim-scripts/pep8.git bundle/pep8
-git submodule init
-git submodule update
-git submodule foreach git submodule init
-git submodule foreach git submodule update
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone git@github.com:altercation/vim-colors-solarized.git ~/.vim/bundle/vim-colors-solarized
 
 BASEDIR=`dirname $0`
 cd `dirname $0`
@@ -57,3 +30,8 @@ if ! grep "source $VIMRC" ~/.vimrc >/dev/null; then
 	cp ~/.vimrc ~/.vimrc.bak
 	echo "source $VIMRC" >> ~/.vimrc
 fi
+
+vim +BundleInstall +qall
+
+cd ~/.vim/bundle/clang_complete
+make
