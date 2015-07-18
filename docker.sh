ef898b425fe59ee6bf0a8b43bfd03be0ed5adfac
Lei Xu <lei@cloudera.com>
2015-07-18 00:40:07 -0700

    fix docker-creation

--- docker.sh
+++ docker.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-ISO_URL="https://github.com/boot2docker/boot2docker/releases/download/v1.6.2/boot2docker.iso"
-
 function docker-create() {
   if [ $# -eq 0 ]; then
     echo "Usage: docker-create <name>"
     exit 1
   fi
   name=$1
-  docker-machine create -d vmwarefusion
+  docker-machine create -d vmwarefusion \
     --vmwarefusion-memory-size "2048" \
     --vmwarefusion-cpu-count "2" \
     $name
