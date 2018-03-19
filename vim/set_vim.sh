ded05a2dfde3f39c6332f31e6bc7daa37e28a0ca
Lei Xu <lei@cloudera.com>
2018-03-18 23:53:51 -0700

    replace to use vim plugin

--- vim/set_vim.sh
+++ vim/set_vim.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 # Setting up vim on a new machine
 
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 ROOTDIR=`dirname $0`
 cd $ROOTDIR/..
 ROOTDIR=`pwd`
 if [ ! -d ~/.vim/bundle ]; then
-	mkdir -p ~/.vim/bundle
+  mkdir -p ~/.vim/bundle
 fi
 if [ ! -d ~/.vim/autoload ]; then
-	mkdir -p ~/.vim/autoload
+  mkdir -p ~/.vim/autoload
 fi
 for d in `find $ROOTDIR/vim -maxdepth 1 -type d`; do
   cp -rf $d ~/.vim
 done
 
 if [ -d ~/.vim/bundle/Vundle.vim ]; then
-	rm -rf ~/.vim/bundle/Vundle.vim
+  rm -rf ~/.vim/bundle/Vundle.vim
 fi
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
@@ -24,20 +28,16 @@ cd `dirname $0`
 
 VIMRC="$ROOTDIR/vim/_vimrc"
 if [ ! -f ~/.vimrc ]; then
-	echo "source $VIMRC" >> ~/.vimrc
+  echo "source $VIMRC" >> ~/.vimrc
 fi
 
 if ! grep "source $VIMRC" ~/.vimrc >/dev/null; then
-	echo "Back up ~/.vimrc to ~/.vimrc.bak "
-	cp ~/.vimrc ~/.vimrc.bak
-	echo "source $VIMRC" >> ~/.vimrc
+  echo "Back up ~/.vimrc to ~/.vimrc.bak "
+  cp ~/.vimrc ~/.vimrc.bak
+  echo "source $VIMRC" >> ~/.vimrc
 fi
 
 vim +BundleInstall +qall
 
 #cd ~/.vim/bundle/clang_complete
 #make
-
-# Set YouCompleteMe
-cd ~/.vim/bundle/YouCompleteMe
-./install.sh --clang-completer
