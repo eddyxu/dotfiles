f8997a12d03b8e3211cdd268dfede2df4ba8651f
Lei Xu <lei@cloudera.com>
2018-03-26 21:45:33 -0700

    set neovim

---
+++ vim/set_nvim.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+pip3 install --upgrade neovim
+mkdir -p ~/.config/nvim
+
+nvim +PlugInstall! +qall
