1565802bc68951c82ab403e0a050c849c59de1f3
Lei Xu <eddyxu@gmail.com>
2018-04-17 13:37:21 -0700

    Install vimrc for neovim to ~/.config/nvim/init.vim

--- vim/set_nvim.sh
+++ vim/set_nvim.sh
@@ -4,5 +4,6 @@ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 pip3 install --upgrade neovim
 mkdir -p ~/.config/nvim
+cp _vimrc ~/.config/nvim/init.vim
 
 nvim +PlugInstall +UpdateRemotePlugins +qa
