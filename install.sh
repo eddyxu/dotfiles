a7d5f214b15a329d8e6054da478c3cb3db91ffb8
Lei Xu <eddyxu@gmail.com>
2020-06-18 10:51:55 -0700

    add kitty.conf

--- install.sh
+++ install.sh
@@ -1,36 +1,11 @@
 #!/usr/bin/env bash
 #
-# Install dotfiles to home directory.
-# Author: Lei Xu <eddyxu@gmail.com>
 
-function link_file {
-    source="${PWD}/$1"
-    target="${HOME}/${1/_/.}"
-
-    if [ -e "${target}" ]; then
-        mv $target $target.bak
-    fi
-
-    ln -sf ${source} ${target}
-}
-
-# Let the home file to source the this configure.
-# Works for bash/vim
-function source_file {
-	srcfile="${PWD}/$1"
-	target="${HOME}/${1/_/.}"
-	if [ ! -e ${target} ] || grep ${srcfile} $target 2>/dev/null; then
-		echo "source ${srcfile}" >> $target
-	fi
-}
-
-#curl -L http://install.ohmyz.sh | sh
+# Zsh
 curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+ln -s -f ${PWD}/_zshrc ~/.zshrc
 
-for i in _*; do
-	if [ $i == '_bashrc' ]; then
-		source_file $i
-		continue
-	fi
-	link_file $i
-done
+
+# Kitty
+mkdir -p ~/.config/kitty
+ln -s -f ${PWD}/kitty.conf ~/.config/kitty
